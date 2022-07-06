b131082aba4dc5af4d96b3d8d08349183a408eff
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2022-07-06 20:59:54 +0200

    Import generated code

--- packages/databricks-sdk-js/src/apis/dbfs.ts
+++ packages/databricks-sdk-js/src/apis/dbfs.ts
@@ -2,39 +2,191 @@
 
 import {ApiClient} from "../api-client";
 
-export interface DbfsPutRequest {
+import * as delegate from "./delegate";
+
+//
+// Enums.
+//
+
+//
+// Subtypes used in request/response types.
+//
+
+export interface FileInfo {
+    path?: string;
+    is_dir?: boolean;
+    file_size?: number;
+    modification_time?: number;
+}
+
+//
+// Request/response types.
+//
+
+export interface ReadRequest {
     path: string;
-    contents: string;
-    overwrite: boolean;
+    offset?: number;
+    length?: number;
 }
 
-export interface DbfsPutResponse {}
+export interface ReadResponse {
+    bytes_read?: number;
+    data?: string;
+}
 
-export interface DbfsDeleteRequest {
+export interface GetStatusRequest {
     path: string;
 }
 
-export interface DbfsDeleteResponse {
-    error_code?: string;
-    message?: string;
+export interface GetStatusResponse {
+    path?: string;
+    is_dir?: boolean;
+    file_size?: number;
+    modification_time?: number;
 }
+
+export interface ListStatusRequest {
+    path: string;
+}
+
+export interface ListStatusResponse {
+    files?: Array<FileInfo>;
+}
+
+export interface PutRequest {
+    path: string;
+    contents?: string;
+    overwrite?: boolean;
+}
+
+export interface PutResponse {}
+
+export interface MkDirsRequest {
+    path: string;
+}
+
+export interface MkDirsResponse {}
+
+export interface MoveRequest {
+    source_path: string;
+    destination_path: string;
+}
+
+export interface MoveResponse {}
+
+export interface DeleteRequest {
+    path: string;
+    recursive?: boolean;
+}
+
+export interface DeleteResponse {}
+
+export interface CreateRequest {
+    path: string;
+    overwrite?: boolean;
+}
+
+export interface CreateResponse {
+    handle?: number;
+}
+
+export interface AddBlockRequest {
+    handle: number;
+    data: string;
+}
+
+export interface AddBlockResponse {}
+
+export interface CloseRequest {
+    handle: number;
+}
+
+export interface CloseResponse {}
 
 export class DbfsService {
-    constructor(readonly client: ApiClient) {}
+    readonly client: ApiClient;
 
-    async put(req: DbfsPutRequest): Promise<DbfsPutResponse> {
+    constructor(client: ApiClient) {
+        this.client = client;
+    }
+
+    async read(request: ReadRequest): Promise<ReadResponse> {
+        return (await this.client.request(
+            "/api/2.0/dbfs/read",
+            "GET",
+            request
+        )) as ReadResponse;
+    }
+
+    async getStatus(request: GetStatusRequest): Promise<GetStatusResponse> {
+        return (await this.client.request(
+            "/api/2.0/dbfs/get-status",
+            "GET",
+            request
+        )) as GetStatusResponse;
+    }
+
+    async list(request: ListStatusRequest): Promise<ListStatusResponse> {
+        return (await this.client.request(
+            "/api/2.0/dbfs/list",
+            "GET",
+            request
+        )) as ListStatusResponse;
+    }
+
+    async put(request: PutRequest): Promise<PutResponse> {
         return (await this.client.request(
             "/api/2.0/dbfs/put",
             "POST",
-            req
-        )) as DbfsPutResponse;
+            request
+        )) as PutResponse;
     }
 
-    async delete(req: DbfsDeleteRequest): Promise<DbfsDeleteResponse> {
+    async mkdirs(request: MkDirsRequest): Promise<MkDirsResponse> {
+        return (await this.client.request(
+            "/api/2.0/dbfs/mkdirs",
+            "POST",
+            request
+        )) as MkDirsResponse;
+    }
+
+    async move(request: MoveRequest): Promise<MoveResponse> {
+        return (await this.client.request(
+            "/api/2.0/dbfs/move",
+            "POST",
+            request
+        )) as MoveResponse;
+    }
+
+    async delete(request: DeleteRequest): Promise<DeleteResponse> {
         return (await this.client.request(
             "/api/2.0/dbfs/delete",
             "POST",
-            req
-        )) as DbfsDeleteResponse;
+            request
+        )) as DeleteResponse;
+    }
+
+    async create(request: CreateRequest): Promise<CreateResponse> {
+        return (await this.client.request(
+            "/api/2.0/dbfs/create",
+            "POST",
+            request
+        )) as CreateResponse;
+    }
+
+    async addBlock(request: AddBlockRequest): Promise<AddBlockResponse> {
+        return (await this.client.request(
+            "/api/2.0/dbfs/add-block",
+            "POST",
+            request
+        )) as AddBlockResponse;
+    }
+
+    async close(request: CloseRequest): Promise<CloseResponse> {
+        return (await this.client.request(
+            "/api/2.0/dbfs/close",
+            "POST",
+            request
+        )) as CloseResponse;
     }
 }
--- packages/databricks-sdk-js/src/apis/libraries.ts
+++ packages/databricks-sdk-js/src/apis/libraries.ts
@@ -1,51 +1,140 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+
 import {ApiClient} from "../api-client";
 
-export interface LibrariesInstallRequest {
-    cluster_id: string;
-    libraries: Array<
-        | {
-              jar: string;
-          }
-        | {
-              egg: string;
-          }
-        | {
-              whl: string;
-          }
-        | {
-              maven: {
-                  coordinates: string;
-                  exclusions?: Array<string>;
-              };
-          }
-        | {
-              pypi: {
-                  package: string;
-                  repo?: string;
-              };
-          }
-        | {
-              cran: {
-                  package: string;
-                  repo?: string;
-              };
-          }
-    >;
+import * as delegate from "./delegate";
+
+//
+// Enums.
+//
+
+export type LibraryInstallStatus =
+    | "PENDING"
+    | "RESOLVING"
+    | "INSTALLING"
+    | "INSTALLED"
+    | "FAILED"
+    | "UNINSTALL_ON_RESTART"
+    | "SKIPPED";
+
+//
+// Subtypes used in request/response types.
+//
+
+export interface PythonPyPiLibrary {
+    package: string;
+    repo?: string;
 }
 
-interface LibrariesInstallResponse {}
+export interface RCranLibrary {
+    package: string;
+    repo?: string;
+}
 
-export class LibrariesApi {
-    constructor(readonly client: ApiClient) {}
+export interface MavenLibrary {
+    coordinates: string;
+    repo?: string;
+    exclusions?: Array<string>;
+}
 
-    async install(
-        req: LibrariesInstallRequest
-    ): Promise<LibrariesInstallResponse> {
+export interface Library {
+    jar?: string;
+    egg?: string;
+    pypi?: PythonPyPiLibrary;
+    maven?: MavenLibrary;
+    cran?: RCranLibrary;
+    whl?: string;
+}
+
+export interface LibraryFullStatus {
+    library?: Library;
+    status?: LibraryInstallStatus;
+    messages?: Array<string>;
+    is_library_for_all_clusters?: boolean;
+}
+
+export interface ClusterLibraryStatuses {
+    cluster_id?: string;
+    library_statuses?: Array<LibraryFullStatus>;
+}
+
+//
+// Request/response types.
+//
+
+export interface ClusterStatusRequest {
+    cluster_id: string;
+}
+
+export interface ClusterStatusResponse {
+    cluster_id?: string;
+    library_statuses?: Array<LibraryFullStatus>;
+}
+
+export interface ListAllClusterLibraryStatusesRequest {}
+
+export interface ListAllClusterLibraryStatusesResponse {
+    statuses?: Array<ClusterLibraryStatuses>;
+}
+
+export interface InstallLibrariesRequest {
+    cluster_id: string;
+    libraries?: Array<Library>;
+}
+
+export interface InstallLibrariesResponse {}
+
+export interface UninstallLibrariesRequest {
+    cluster_id: string;
+    libraries?: Array<Library>;
+}
+
+export interface UninstallLibrariesResponse {}
+
+export class ManagedLibraryService {
+    readonly client: ApiClient;
+
+    constructor(client: ApiClient) {
+        this.client = client;
+    }
+
+    async clusterStatus(
+        request: ClusterStatusRequest
+    ): Promise<ClusterStatusResponse> {
+        return (await this.client.request(
+            "/api/2.0/libraries/cluster-status",
+            "GET",
+            request
+        )) as ClusterStatusResponse;
+    }
+
+    async allClusterStatuses(
+        request: ListAllClusterLibraryStatusesRequest
+    ): Promise<ListAllClusterLibraryStatusesResponse> {
+        return (await this.client.request(
+            "/api/2.0/libraries/all-cluster-statuses",
+            "GET",
+            request
+        )) as ListAllClusterLibraryStatusesResponse;
+    }
+
+    async installLibraries(
+        request: InstallLibrariesRequest
+    ): Promise<InstallLibrariesResponse> {
         return (await this.client.request(
             "/api/2.0/libraries/install",
             "POST",
-            req
-        )) as LibrariesInstallResponse;
+            request
+        )) as InstallLibrariesResponse;
+    }
+
+    async uninstallLibraries(
+        request: UninstallLibrariesRequest
+    ): Promise<UninstallLibrariesResponse> {
+        return (await this.client.request(
+            "/api/2.0/libraries/uninstall",
+            "POST",
+            request
+        )) as UninstallLibrariesResponse;
     }
 }
