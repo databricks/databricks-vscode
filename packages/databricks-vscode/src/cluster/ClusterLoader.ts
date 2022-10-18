61caa89a6079ae927a3a36c221115204c017f292
kartikgupta-db <kartik.gupta@databricks.com>
2022-10-18 18:47:39 +0200

    comments

--- packages/databricks-vscode/src/cluster/ClusterLoader.ts
+++ packages/databricks-vscode/src/cluster/ClusterLoader.ts
@@ -167,6 +167,12 @@ export class ClusterLoader implements Disposable {
                 await this._load();
             } catch (e) {
                 let err = e;
+
+                /*
+                Standard Error class has message and stack fields set as non enumerable.
+                To correctly account for all such fields, we iterate over all own-properties of
+                the error object and accumulate them as enumerable fields in the final err object.
+                */
                 if (Object(err) === err) {
                     err = {
                         ...Object.getOwnPropertyNames(err).reduce((acc, i) => {
--- packages/databricks-vscode/src/extension.ts
+++ packages/databricks-vscode/src/extension.ts
@@ -44,6 +44,11 @@ export function activate(context: ExtensionContext): PublicApi {
         true
     );
 
+    /** 
+    This logger collects all the logs in the extension.
+    
+    TODO Make this logger log to a seperate (or common?) output console in vscode
+    */
     NamedLogger.getOrCreate(
         "Extension",
         {
