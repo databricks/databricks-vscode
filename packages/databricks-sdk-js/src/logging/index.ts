dc5654afbfd08bedad942d155d4597f2fe8ff19d
kartikgupta-db <kartik.gupta@databricks.com>
2022-10-25 20:33:23 +0530

    true rename

--- packages/databricks-sdk-js/src/logging/index.ts
+++ packages/databricks-sdk-js/src/logging/index.ts
@@ -1,5 +1,5 @@
 export * from "./loggingDecorators";
 export * from "./NamedLogger";
 export * from "./DefaultLogger";
-export * from "./exposedLoggers";
+export * from "./ExposedLoggers";
 export * from "./types";
