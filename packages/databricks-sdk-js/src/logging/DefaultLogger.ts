e0d3996c640e4d9ffa786accb10b5ab8d2e0ff94
kartikgupta-db <kartik.gupta@databricks.com>
2022-10-27 00:54:22 +0530

    Don't log to stdout/stderr by default from SDK

--- packages/databricks-sdk-js/src/logging/DefaultLogger.ts
+++ packages/databricks-sdk-js/src/logging/DefaultLogger.ts
@@ -1,10 +1,21 @@
-import {Writable} from "stream";
+import {Transform, Writable} from "stream";
 import {LogEntry, Logger} from "./types";
 
 export class DefaultLogger implements Logger {
     private _stream: Writable;
     constructor(outputStream?: Writable) {
-        this._stream = outputStream ?? process.stderr;
+        //set to noop if no stream specified
+        this._stream =
+            outputStream ??
+            new (class extends Writable {
+                _write(
+                    chunk: any,
+                    encoding: BufferEncoding,
+                    callback: (error?: Error | null | undefined) => void
+                ): void {
+                    callback();
+                }
+            })();
     }
 
     log(level: string, message?: string, obj?: any) {
