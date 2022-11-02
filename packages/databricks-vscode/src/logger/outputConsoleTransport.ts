a9406638de9041503eb12823945af44d66946874
kartikgupta-db <kartik.gupta@databricks.com>
2022-11-02 23:58:11 +0530

    Truncate arrays beautifully

--- packages/databricks-vscode/src/logger/outputConsoleTransport.ts
+++ packages/databricks-vscode/src/logger/outputConsoleTransport.ts
@@ -2,27 +2,71 @@ import {OutputChannel, workspace} from "vscode";
 import {transports, format} from "winston";
 import {OutputConsoleStream} from "./OutputConsoleStream";
 import {LEVEL, MESSAGE, SPLAT} from "triple-beam";
+import {info} from "console";
+
+const workspaceConfigs = {
+    get maxFieldLength() {
+        return (
+            workspace
+                .getConfiguration("databricks")
+                ?.get<number>("logs.maxFieldLength") ?? 40
+        );
+    },
+    get truncationDepth() {
+        return (
+            workspace
+                .getConfiguration("databricks")
+                ?.get<number>("logs.truncationDepth") ?? 2
+        );
+    },
+    get maxArrayLength() {
+        return (
+            workspace
+                .getConfiguration("databricks")
+                ?.get<number>("logs.maxArrayLength") ?? 2
+        );
+    },
+};
+
+function processPrimitiveOrString(obj: any) {
+    let valueStr: string;
+    if (Object(obj) !== obj) {
+        valueStr = typeof obj === "string" ? obj : String(obj).toString();
+    } else {
+        valueStr = JSON.stringify(obj);
+    }
+
+    return valueStr.length > workspaceConfigs.maxFieldLength
+        ? `${valueStr.slice(0, workspaceConfigs.maxFieldLength)} ...(${
+              valueStr.length - workspaceConfigs.maxFieldLength
+          } bytes more)`
+        : valueStr;
+}
+
+function processArray(obj: Array<any>, depth: number): Array<any> {
+    const finalArr = [];
+    for (let child of obj) {
+        finalArr.push(recursiveTruncate(child, depth - 1));
+        if (finalArr.length === workspaceConfigs.maxArrayLength) {
+            break;
+        }
+    }
+    if (obj.length > workspaceConfigs.maxArrayLength) {
+        finalArr.push(
+            `...${obj.length - workspaceConfigs.maxArrayLength} more items`
+        );
+    }
+    return finalArr;
+}
 
 function recursiveTruncate(obj: any, depth: number) {
-    const maxFieldLength =
-        workspace
-            .getConfiguration("databricks.logs")
-            ?.get<number>("maxFieldLength") ?? 40;
-
     //If object is of primitive type
-    if (Object(obj) !== obj || Array.isArray(obj) || depth === 0) {
-        let valueStr: string;
-        if (Object(obj) !== obj) {
-            valueStr = typeof obj === "string" ? obj : String(obj).toString();
-        } else {
-            valueStr = JSON.stringify(obj);
-        }
+    if (Object(obj) !== obj || depth === 0) {
+        return processPrimitiveOrString(obj);
+    }
 
-        return valueStr.length > maxFieldLength
-            ? `${valueStr.slice(0, maxFieldLength)} ...(${
-                  valueStr.length - maxFieldLength
-              } bytes more)`
-            : valueStr;
+    if (Array.isArray(obj)) {
+        return processArray(obj, depth);
     }
 
     obj = Object.assign({}, obj);
@@ -36,7 +80,6 @@ function recursiveTruncate(obj: any, depth: number) {
 export function getOutputConsoleTransport(outputChannel: OutputChannel) {
     return new transports.Stream({
         format: format.combine(
-            format.timestamp(),
             format((info) => {
                 const stripped = Object.assign({}, info) as any;
                 if (stripped[LEVEL] === "error") {
@@ -47,16 +90,17 @@ export function getOutputConsoleTransport(outputChannel: OutputChannel) {
                 delete stripped[SPLAT];
                 delete stripped["level"];
                 delete stripped["message"];
-                delete stripped["timestamp"];
 
-                return recursiveTruncate(
-                    stripped,
-                    workspace
-                        .getConfiguration("databricks.logs")
-                        ?.get<number>("truncationDepth") ?? 2
-                );
+                return {
+                    ...info,
+                    ...recursiveTruncate(
+                        stripped,
+                        workspaceConfigs.truncationDepth
+                    ),
+                    timestamp: new Date().toLocaleString(),
+                };
             })(),
-            format.prettyPrint({depth: 2})
+            format.prettyPrint({depth: workspaceConfigs.truncationDepth})
         ),
         stream: new OutputConsoleStream(outputChannel, {
             defaultEncoding: "utf-8",
