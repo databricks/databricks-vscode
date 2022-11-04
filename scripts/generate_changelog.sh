6668716820e11084c6caa331d1623968d389f090
kartikgupta-db <kartik.gupta@databricks.com>
2022-11-04 16:42:47 +0530

    read the first var

--- scripts/generate_changelog.sh
+++ scripts/generate_changelog.sh
@@ -1,12 +1,12 @@
 for PACKAGE in "packages/databricks-vscode" "packages/databricks-sdk" "packages/databricks-vscode-types" "."; do
-    echo "## $PACKAGE" >> $2
+    echo "## $PACKAGE" >> $1
 
     TAG=$(git describe --abbrev=0 --match "release-v*")
     if [ $? -ne 0 ]; then
         echo "No release tag matching pattern 'release-v*' found. Generating changelog from beggining"
-        yarn conventional-changelog -k $PACKAGE --commit-path $PACKAGE >> $2
+        yarn conventional-changelog -k $PACKAGE --commit-path $PACKAGE >> $1
     else
         echo "Release tag found. Generating changelog from $TAG"
-        yarn conventional-changelog --tag-prefix="release-v" -k $PACKAGE --commit-path $PACKAGE >> $2
+        yarn conventional-changelog --tag-prefix="release-v" -k $PACKAGE --commit-path $PACKAGE >> $1
     fi
 done
