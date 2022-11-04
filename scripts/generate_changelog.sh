8e31e33b33e1c4f3b116215ab25d205f1da52170
kartikgupta-db <kartik.gupta@databricks.com>
2022-11-04 22:52:37 +0530

    Release pr job + tester

---
+++ scripts/generate_changelog.sh
@@ -0,0 +1,12 @@
+for PACKAGE in "packages/databricks-vscode" "packages/databricks-sdk" "packages/databricks-vscode-types" "."; do
+    echo "## $PACKAGE" >> $1
+
+    TAG=$(git describe --abbrev=0 --match "release-v*")
+    if [ $? -ne 0 ]; then
+        echo "No release tag matching pattern 'release-v*' found. Generating changelog from beggining"
+        yarn conventional-changelog -k $PACKAGE --commit-path $PACKAGE >> $1
+    else
+        echo "Release tag found. Generating changelog from $TAG"
+        yarn conventional-changelog --tag-prefix="release-v" -k $PACKAGE --commit-path $PACKAGE >> $1
+    fi
+done
