8beee5619170bf44dc9fd13a8f0476ec21156620
kartikgupta-db <kartik.gupta@databricks.com>
2022-11-10 17:26:13 +0100

    Escape fix

--- scripts/generate_changelog.sh
+++ scripts/generate_changelog.sh
@@ -1,12 +1,16 @@
-for PACKAGE in "packages/databricks-vscode" "packages/databricks-sdk" "packages/databricks-vscode-types" "."; do
-    echo "## $PACKAGE" >> $1
+for PACKAGE in "packages/databricks-vscode" "packages/databricks-sdk-js" "packages/databricks-vscode-types"; do
+    tmpfile=$(mktemp /tmp/generate_changelog.XXXXXX)
+    echo "## $PACKAGE" >> $tmpfile
 
     TAG=$(git describe --abbrev=0 --match "release-v*")
     if [ $? -ne 0 ]; then
         echo "No release tag matching pattern 'release-v*' found. Generating changelog from begining"
-        yarn conventional-changelog -k $PACKAGE --commit-path $PACKAGE >> $1
+        yarn conventional-changelog -k $PACKAGE --commit-path $PACKAGE >> $tmpfile
     else
         echo "Release tag found. Generating changelog from $TAG"
-        yarn conventional-changelog --tag-prefix="release-v" -k $PACKAGE --commit-path $PACKAGE >> $1
+        yarn conventional-changelog --tag-prefix="release-v" -k $PACKAGE --commit-path $PACKAGE >> $tmpfile
     fi
+
+    cat $tmpfile >> $1
+    cat $tmpfile >> $PACKAGE/CHANGELOG.md
 done
