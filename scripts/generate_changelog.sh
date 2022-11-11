93167d3df08b6cc7d007fda4ec7f756ac5170cfa
Kartik Gupta <88345179+kartikgupta-db@users.noreply.github.com>
2022-11-11 14:40:20 +0100

    Fix changelog generation and release pipeline (#199)
    2 step release process
    1. Create Release PR job with custom version
    https://github.com/databricks/databricks-vscode/actions/runs/3444623300/jobs/5747421099
    2. Merge the release PR
    https://github.com/databricks/databricks-vscode/pull/212
    3. Create Release job runs and creates draft release
    https://github.com/databricks/databricks-vscode/releases/tag/untagged-7eb796d455d904b38cb3

--- scripts/generate_changelog.sh
+++ scripts/generate_changelog.sh
@@ -1,12 +1,17 @@
-for PACKAGE in "packages/databricks-vscode" "packages/databricks-sdk" "packages/databricks-vscode-types" "."; do
-    echo "## $PACKAGE" >> $1
+for PACKAGE in "packages/databricks-vscode" "packages/databricks-sdk-js" "packages/databricks-vscode-types"; do
+    tmpfile=$(mktemp /tmp/generate_changelog.XXXXXX)
+    echo "## $PACKAGE" >> $tmpfile
 
     TAG=$(git describe --abbrev=0 --match "release-v*")
     if [ $? -ne 0 ]; then
         echo "No release tag matching pattern 'release-v*' found. Generating changelog from begining"
-        yarn conventional-changelog -k $PACKAGE --commit-path $PACKAGE >> $1
+        yarn conventional-changelog -k $PACKAGE --commit-path $PACKAGE >> $tmpfile
     else
         echo "Release tag found. Generating changelog from $TAG"
-        yarn conventional-changelog --tag-prefix="release-v" -k $PACKAGE --commit-path $PACKAGE >> $1
+        yarn conventional-changelog --tag-prefix="release-v" -k $PACKAGE --commit-path $PACKAGE >> $tmpfile
     fi
+
+    cat $tmpfile | grep -Ev "Release: v.+" >> $2
+    echo "# Release: v$1" >> $PACKAGE/CHANGELOG.md
+    cat $tmpfile | grep -Ev "Release: v.+" >> $PACKAGE/CHANGELOG.md
 done
