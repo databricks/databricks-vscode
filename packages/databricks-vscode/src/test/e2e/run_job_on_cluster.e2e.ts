9d9f9a9b07ed617fe8033e8611f16bc997a97390
kartikgupta-db <kartik.gupta@databricks.com>
2022-12-14 15:25:03 +0100

    Run file as job

--- packages/databricks-vscode/src/test/e2e/run_job_on_cluster.e2e.ts
+++ packages/databricks-vscode/src/test/e2e/run_job_on_cluster.e2e.ts
@@ -2,10 +2,11 @@ import path from "node:path";
 import * as fs from "fs/promises";
 import assert from "node:assert";
 import {getViewSection, getViewSubSection, waitForTreeItems} from "./utils";
-import {sleep} from "wdio-vscode-service";
+import {sleep, TreeItem} from "wdio-vscode-service";
 
-describe("Run python on cluster", () => {
+describe("Run python on cluster", async function () {
     let projectDir: string;
+    this.timeout(2 * 60 * 1000);
 
     before(async () => {
         assert(process.env.TEST_DEFAULT_CLUSTER_ID);
@@ -24,7 +25,7 @@ describe("Run python on cluster", () => {
             })
         );
         await fs.writeFile(
-            path.join(projectDir, "hello.py"),
+            path.join(projectDir, "file.py"),
             `spark.sql('SELECT "hello world"').show()`
         );
 
@@ -35,37 +36,62 @@ describe("Run python on cluster", () => {
                 `spark.sql('SELECT "hello world"').show()`,
             ].join("\n")
         );
-    });
 
-    it("should connect to Databricks", async () => {
         const section = await getViewSection("CONFIGURATION");
         assert(section);
         await waitForTreeItems(section);
     });
 
-    it("should start syncing", async () => {
+    beforeEach(async () => {
         const section = await getViewSection("CLUSTERS");
         await section?.collapse();
 
         const repoConfigItem = await getViewSubSection("CONFIGURATION", "Repo");
         assert(repoConfigItem);
-        const buttons = await repoConfigItem.getActionButtons();
-        await buttons[0].elem.click();
 
-        // wait for sync to finish
-        const workbench = await driver.getWorkbench();
-        const terminalView = await workbench.getBottomBar().openTerminalView();
-
-        while (true) {
-            await sleep(500);
-            const text = await terminalView.getText();
-            if (text.includes("Sync Complete")) {
+        let status: TreeItem | undefined = undefined;
+        for (const i of await repoConfigItem.getChildren()) {
+            if ((await i.getLabel()).includes("State:")) {
+                status = i;
                 break;
             }
         }
+        assert(status);
+        if ((await status.getDescription())?.includes("STOPPED")) {
+            const buttons = await repoConfigItem.getActionButtons();
+            await buttons[0].elem.click();
+        }
+
+        await browser.waitUntil(
+            async () => {
+                const repoConfigItem = await getViewSubSection(
+                    "CONFIGURATION",
+                    "Repo"
+                );
+                assert(repoConfigItem);
+
+                status = undefined;
+                for (const i of await repoConfigItem.getChildren()) {
+                    if ((await i.getLabel()).includes("State:")) {
+                        status = i;
+                        break;
+                    }
+                }
+                assert(status);
+                const description = await status?.getDescription();
+                return (
+                    description !== undefined &&
+                    description.includes("WATCHING_FOR_CHANGES")
+                );
+            },
+            {
+                timeout: 20000,
+                timeoutMsg: "Couldn't finish sync in 20s",
+            }
+        );
     });
 
-    it("should run a python notebook on a cluster", async () => {
+    it("should run a python notebook as a job on a cluster", async () => {
         const workbench = await driver.getWorkbench();
         const editorView = workbench.getEditorView();
         await editorView.closeAllEditors();
@@ -96,12 +122,6 @@ describe("Run python on cluster", () => {
         };
         /* eslint-enable @typescript-eslint/naming-convention */
 
-        Object.keys(labelToDefaults).forEach(async (key) => {
-            await expect($(`aria/${labelToDefaults[key].label}`)).toHaveText(
-                labelToDefaults[key].default
-            );
-        });
-
         // wait for job to get a task id
         await browser.waitUntil(
             async () =>
@@ -131,11 +151,68 @@ describe("Run python on cluster", () => {
             }
         );
 
-        Object.keys(labelToDefaults).forEach(async (key) => {
-            await expect(
-                $(`aria/${labelToDefaults[key].label}`)
-            ).not.toHaveText(labelToDefaults[key].default);
-        });
+        webView.close();
+    });
+
+    it("should run a python file as a job on a cluster", async () => {
+        const workbench = await driver.getWorkbench();
+        const editorView = workbench.getEditorView();
+        await editorView.closeAllEditors();
+
+        // open file
+        const input = await workbench.openCommandPrompt();
+        await sleep(200);
+        await input.setText("file.py");
+        await input.confirm();
+        await sleep(500);
+
+        // run file
+        await workbench.executeQuickPick(
+            "Databricks: Run File as Workflow on Databricks"
+        );
+
+        const webView = await workbench.getWebviewByTitle(/Databricks Job Run/);
+        await webView.open();
+
+        /* eslint-disable @typescript-eslint/naming-convention */
+        const labelToDefaults = {
+            taskRunId: {label: "task-run-id", default: /N\\A/},
+            clusterId: {label: "cluster", default: /N\\A/},
+            startTime: {label: "run-start-time", default: /-/},
+            endTime: {label: "run-end-time", default: /-/},
+            duration: {label: "run-duration", default: /-/},
+            status: {label: "run-status", default: /Synchronizing/},
+        };
+        /* eslint-enable @typescript-eslint/naming-convention */
+
+        // wait for job to get a task id
+        await browser.waitUntil(
+            async () =>
+                (
+                    await browser.getTextByLabel(
+                        labelToDefaults.taskRunId.label
+                    )
+                ).match(labelToDefaults.taskRunId.default) === null,
+            {
+                timeoutMsg: "Job did not start",
+            }
+        );
+
+        expect(
+            await browser.getTextByLabel(labelToDefaults.startTime.label)
+        ).not.toHaveText(labelToDefaults.startTime.default);
+
+        await browser.waitUntil(
+            async () =>
+                (
+                    await browser.getTextByLabel(labelToDefaults.status.label)
+                ).includes("Succeeded"),
+            {
+                timeout: 20000,
+                interval: 100,
+                timeoutMsg: "Job did not reach succeeded status after 20s.",
+            }
+        );
 
         webView.close();
     });
