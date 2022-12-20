aa1fdb35cd8858ce7a28186abeda32ae70ccebf9
kartikgupta-db <kartik.gupta@databricks.com>
2022-12-20 11:52:18 +0100

    rename test spec

--- packages/databricks-vscode/src/test/e2e/run_job_on_cluster.e2e.ts
+++ packages/databricks-vscode/src/test/e2e/run_job_on_cluster.e2e.ts
@@ -10,7 +10,7 @@ import {
 } from "./utils";
 import {sleep} from "wdio-vscode-service";
 
-describe("Run python on cluster", async function () {
+describe("Run job on cluster", async function () {
     let projectDir: string;
     this.timeout(2 * 60 * 1000);
 
