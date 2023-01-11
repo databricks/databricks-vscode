5fbe525b44a07a4df5ce28682760eb9522047a17
Fabian Jakobs <fabian.jakobs@databricks.com>
2023-01-11 08:35:09 +0000

    Use typescript project references (#356)
    - `yarn watch` works across packages
    - goto definition works across packages
    - faster incremental builds
    - unify `tsconfig.json`s
    
    See https://www.typescriptlang.org/docs/handbook/project-references.html

--- packages/databricks-sdk-js/src/fetch.js
+++ packages/databricks-sdk-js/src/fetch.js
@@ -1,7 +1,7 @@
 // node-fetch@3 is ESM only. Need some trickery to make it work with CommonJS
-
-exports.fetch = async function (url, init) {
-    const {default: fetch} = await import("node-fetch");
+exports.fetch = async function fetch(url, init) {
+    // hide `import` statement from TypeScript compiler so it doesn't mess with it
+    const {default: fetch} = await eval(`import("node-fetch")`);
     const controller = new AbortController();
     const signal = controller.signal;
     return {
