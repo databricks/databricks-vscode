03594474457fccd64efbb3ee9d5f0023fe4d1c99
Fabian Jakobs <fabian.jakobs@databricks.com>
2023-01-12 13:36:17 +0100

    no need to pull from go SDK repo

--- packages/databricks-sdk-js/scripts/generate_openapi.sh
+++ packages/databricks-sdk-js/scripts/generate_openapi.sh
@@ -7,8 +7,4 @@ GO_SDK_HASH=${1:-main}
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 OPENAPI=$DIR/all-internal.json
 
-pushd $DIR/../../../databricks-sdk-go
-git checkout $GO_SDK_HASH || git pull && git checkout $GO_SDK_HASH
-git pull
-go run openapi/gen/main.go -spec $OPENAPI -target $DIR/src
-popd
+go run github.com/databricks/databricks-sdk-go/openapi/gen@$GO_SDK_HASH -spec $OPENAPI -target $DIR/src
