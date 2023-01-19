bbb4131901217f1d7fa0363e11e25d22f0de8386
Kartik Gupta <88345179+kartikgupta-db@users.noreply.github.com>
2023-01-19 12:05:51 +0100

    Add SDK support for Files in Workspace (#366)
    Co-authored-by: Fabian Jakobs <fabian.jakobs@databricks.com>

---
+++ packages/databricks-sdk-js/src/services/wsfs/WorkspaceFsDir.ts
@@ -0,0 +1,57 @@
+import {posix} from "path";
+import {ApiClientResponseError} from "../../api-client";
+import {Context} from "../../context";
+import {WorkspaceFsEntity} from ".";
+
+export class WorkspaceFsDir extends WorkspaceFsEntity {
+    override async generateUrl(host: URL): Promise<string> {
+        return `${host.host}#folder/${this.details.object_id}`;
+    }
+
+    public getAbsoluteChildPath(path: string) {
+        const resolved = posix.resolve(this.path, path);
+        const relative = posix.relative(this.path, resolved);
+
+        if (
+            !posix.isAbsolute(relative) &&
+            !relative.startsWith(".." + posix.sep)
+        ) {
+            return resolved;
+        }
+
+        return undefined;
+    }
+
+    protected override async _mkdir(
+        path: string,
+        ctx?: Context
+    ): Promise<WorkspaceFsEntity | undefined> {
+        const validPath = this.getAbsoluteChildPath(path);
+        if (!validPath) {
+            const err = new Error(
+                `Can't create ${path} as child of ${this.path}: Invalid path`
+            );
+            ctx?.logger?.error(`Can't create child of ${path}`, err);
+            throw err;
+        }
+
+        try {
+            await this._workspaceFsService.mkdirs({path: validPath});
+        } catch (e: unknown) {
+            let err: any = e;
+            if (e instanceof ApiClientResponseError) {
+                if (e.error_code === "RESOURCE_ALREADY_EXISTS") {
+                    err = new Error(
+                        `Can't create ${path} as child of ${this.path}: A file with same path exists`
+                    );
+                }
+            }
+            ctx?.logger?.error(`Can't create child of ${path}`, err);
+            throw err;
+        }
+
+        return await WorkspaceFsEntity.fromPath(this.wsClient, validPath, ctx);
+    }
+}
+
+export class WorkspaceFsRepo extends WorkspaceFsDir {}
---
+++ packages/databricks-sdk-js/src/services/wsfs/WorkspaceFsEntity.ts
@@ -0,0 +1,200 @@
+import {posix} from "path";
+import {ApiClientResponseError} from "../../api-client";
+import {ObjectInfo, WorkspaceService} from "../../apis/workspace";
+import {context, Context} from "../../context";
+import {ExposedLoggers, withLogContext} from "../../logging";
+import {
+    WorkspaceFsDir,
+    WorkspaceFsRepo,
+    WorkspaceFsFile,
+    WorkspaceFsNotebook,
+} from ".";
+import {WorkspaceClient} from "../../WorkspaceClient";
+
+export class ObjectInfoValidationError extends Error {
+    constructor(message: string, readonly details: ObjectInfo) {
+        super(message);
+    }
+}
+
+/* eslint-disable @typescript-eslint/naming-convention */
+class RequiredFields {
+    "object_id" = "";
+    "object_type" = "";
+    "path" = "";
+}
+/* eslint-enable @typescript-eslint/naming-convention */
+
+type RequiredObjectInfo = ObjectInfo & RequiredFields;
+
+export abstract class WorkspaceFsEntity {
+    protected _workspaceFsService: WorkspaceService;
+    private _children?: Array<WorkspaceFsEntity>;
+    private _details: RequiredObjectInfo;
+
+    constructor(
+        protected readonly wsClient: WorkspaceClient,
+        details: ObjectInfo
+    ) {
+        this._workspaceFsService = wsClient.workspace;
+        this._details = this.validateDetails(details);
+    }
+
+    @withLogContext(ExposedLoggers.SDK)
+    private validateDetails(
+        details: ObjectInfo,
+        @context ctx?: Context
+    ): RequiredObjectInfo {
+        Object.keys(new RequiredFields()).forEach((field) => {
+            if (details[field as keyof ObjectInfo] === undefined) {
+                const err = new ObjectInfoValidationError(
+                    `These fields are required for fs objects (${Object.keys(
+                        new RequiredFields()
+                    ).join(", ")})`,
+                    details
+                );
+                ctx?.logger?.error("ObjectInfo validation error", err);
+                throw err;
+            }
+        });
+
+        return details as RequiredObjectInfo;
+    }
+
+    set details(details: ObjectInfo) {
+        this._details = this.validateDetails(details);
+    }
+
+    get details() {
+        return this._details;
+    }
+
+    get path() {
+        return this._details.path;
+    }
+
+    protected abstract generateUrl(host: URL): Promise<string>;
+
+    get url() {
+        return new Promise<string>((resolve) => {
+            this.wsClient.apiClient.host.then((host) =>
+                resolve(this.generateUrl(host))
+            );
+        });
+    }
+
+    get type() {
+        return this._details.object_type;
+    }
+
+    get id() {
+        return this._details.object_id;
+    }
+
+    protected async fetchChildren() {
+        function objIsDefined(
+            obj: WorkspaceFsEntity | undefined
+        ): obj is WorkspaceFsEntity {
+            return obj !== undefined ? true : false;
+        }
+
+        this._children = (
+            await this._workspaceFsService.list({path: this.path})
+        ).objects
+            ?.map((obj) => entityFromObjInfo(this.wsClient, obj))
+            .filter(objIsDefined);
+    }
+
+    @withLogContext(ExposedLoggers.SDK)
+    async refresh(@context ctx?: Context) {
+        this._children = undefined;
+
+        try {
+            const details = await this._workspaceFsService.getStatus(
+                {
+                    path: this.path,
+                },
+                ctx
+            );
+
+            this.details = details;
+            return this;
+        } catch (e: unknown) {
+            if (e instanceof ApiClientResponseError) {
+                if (e.error_code === "RESOURCE_DOES_NOT_EXIST") {
+                    return undefined;
+                }
+            }
+        }
+    }
+
+    get children() {
+        return new Promise<Array<WorkspaceFsEntity>>((resolve) => {
+            if (this._children === undefined) {
+                this.fetchChildren().then(() => resolve(this._children ?? []));
+            } else {
+                resolve(this._children);
+            }
+        });
+    }
+
+    @withLogContext(ExposedLoggers.SDK, "WorkspaceFsEntity.fromPath")
+    static async fromPath(
+        wsClient: WorkspaceClient,
+        path: string,
+        @context ctx?: Context
+    ) {
+        try {
+            const entity = entityFromObjInfo(
+                wsClient,
+                await wsClient.workspace.getStatus({path}, ctx)
+            );
+            return entity;
+        } catch (e) {
+            if (e instanceof ApiClientResponseError) {
+                if (e.error_code === "RESOURCE_DOES_NOT_EXIST") {
+                    return undefined;
+                }
+            }
+        }
+    }
+
+    get parent(): Promise<WorkspaceFsEntity | undefined> {
+        const parentPath = posix.dirname(this.path);
+        return WorkspaceFsEntity.fromPath(this.wsClient, parentPath);
+    }
+
+    get basename(): string {
+        return posix.basename(this.path);
+    }
+
+    protected abstract _mkdir(
+        path: string,
+        ctx?: Context
+    ): Promise<WorkspaceFsEntity | undefined>;
+
+    @withLogContext(ExposedLoggers.SDK)
+    async mkdir(
+        path: string,
+        @context ctx?: Context
+    ): Promise<WorkspaceFsEntity | undefined> {
+        return this._mkdir(path, ctx);
+    }
+}
+
+function entityFromObjInfo(
+    wsClient: WorkspaceClient,
+    details: ObjectInfo
+): WorkspaceFsEntity | undefined {
+    switch (details.object_type) {
+        case "DIRECTORY":
+            return new WorkspaceFsDir(wsClient, details);
+        case "REPO":
+            return new WorkspaceFsRepo(wsClient, details);
+        case "FILE":
+            return new WorkspaceFsFile(wsClient, details);
+        case "NOTEBOOK":
+            return new WorkspaceFsNotebook(wsClient, details);
+    }
+    return undefined;
+}
