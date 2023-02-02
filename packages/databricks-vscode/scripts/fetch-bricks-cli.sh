9d485700eadd041c8a951905b856e561908c22bd
Fabian Jakobs <fabian.jakobs@databricks.com>
2023-02-02 13:13:23 +0000

    Fetch bricks from S3 (#416)

--- packages/databricks-vscode/scripts/fetch-bricks-cli.sh
+++ packages/databricks-vscode/scripts/fetch-bricks-cli.sh
@@ -10,7 +10,7 @@ fi
 
 BRICKS_DIR=$(mktemp -d -t bricks-XXXXXXXXXX)
 pushd $BRICKS_DIR
-gh release download $BRICKS_VERSION -R databricks/bricks -p "*$BRICKS_ARCH.zip"
+curl https://databricks-bricks.s3.amazonaws.com/v${BRICKS_VERSION}/bricks_${BRICKS_VERSION}_${BRICKS_ARCH}.zip -o bricks_${BRICKS_VERSION}_${BRICKS_ARCH}.zip
 unzip bricks_*_$BRICKS_ARCH.zip
 rm bricks_*_$BRICKS_ARCH.zip
 ls
