eef535eb53d3df3bd4690cad19af6e7f35a8e419
Fabian Jakobs <fabian.jakobs@databricks.com>
2023-02-17 16:10:36 +0100

    lint

--- packages/databricks-vscode/src/configuration/AzureCliCheck.ts
+++ packages/databricks-vscode/src/configuration/AzureCliCheck.ts
@@ -46,10 +46,7 @@ async function orchestrate<S, KEYS extends string>(
             throw new OrchestrationLoopError();
         }
         const result: StepResult<S, KEYS> = await steps[step]();
-        logger?.info(
-            `Azire CLI check: ${step}`,
-            result
-        );
+        logger?.info(`Azire CLI check: ${step}`, result);
 
         if (result.type === "error") {
             throw result.error;
