e488ea8cf0434e9b9ed525bc04b62f4fa640b7c8
kartikgupta-db <kartik.gupta@databricks.com>
2023-07-03 15:15:52 +0200

    address feedback

--- packages/databricks-vscode/resources/python/00-databricks-init.py
+++ packages/databricks-vscode/resources/python/00-databricks-init.py
@@ -2,7 +2,7 @@ from databricks.sdk.runtime import dbutils
 from databricks.connect import DatabricksSession
 from pyspark.sql import SparkSession, functions as udf
 
-# "display", "displayHTML", "dbutils", "table", "sql", "udf", "getArgument", "sc", "sqlContext", "spark"
+# "displayHTML", "table", "sc", "sqlContext" are missing
 spark: SparkSession = DatabricksSession.builder.getOrCreate()
 sql = spark.sql
 getArgument = dbutils.widgets.getArgument
