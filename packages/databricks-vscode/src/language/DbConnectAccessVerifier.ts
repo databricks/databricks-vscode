4cef8eeab822d118e3c9f0cfbc5030bfac3b26e2
kartikgupta-db <kartik.gupta@databricks.com>
2023-09-21 16:49:50 +0200

    Fix invalid access mode prompt

--- packages/databricks-vscode/src/language/DbConnectAccessVerifier.ts
+++ packages/databricks-vscode/src/language/DbConnectAccessVerifier.ts
@@ -92,7 +92,7 @@ export class DbConnectAccessVerifier extends MultiStepAccessVerifier {
                 "checkCluster",
                 `Cluster doesn't have UC enabled.`,
                 this.promptForAttachingCluster(
-                    `Databricks Connect requires Access Mode to be "Single User" or "Shared"). Currently it is ${
+                    `Databricks Connect requires cluster Access Mode to be "Single User" or "Shared"). Currently it is ${
                         cluster.accessMode ?? "custom"
                     }. Please attach a new cluster.`
                 )
