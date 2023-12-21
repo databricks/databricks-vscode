9f2f05755632ceaf2de55dd5dcd7ff86597f21fa
Ilia Babanov <ilia.babanov@databricks.com>
2023-12-21 14:02:52 +0100

    Update the message

--- packages/databricks-vscode/src/cli/CliWrapper.ts
+++ packages/databricks-vscode/src/cli/CliWrapper.ts
@@ -125,8 +125,7 @@ export class CliWrapper {
                         "Failed to parse Databricks Config File, please make sure it's in the correct ini format";
                 } else if (e.message.includes("spawn UNKNOWN")) {
                     msg =
-                        "Failed to parse Databricks Config File using databricks CLI, please make sure you have permissions to execute this binary: " +
-                        this.cliPath;
+                        `Failed to parse Databricks Config File using databricks CLI, please make sure you have permissions to execute this binary: "${this.cliPath}"`;
                 }
             }
             ctx?.logger?.error(msg, e);
