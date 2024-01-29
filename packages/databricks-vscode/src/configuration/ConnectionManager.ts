2e33f0e50223bea7064baf9bfcd1a6cbcd91bc4c
Ilia Babanov <ilia.babanov@databricks.com>
2024-01-29 16:25:03 +0100

    Code style

--- packages/databricks-vscode/src/configuration/ConnectionManager.ts
+++ packages/databricks-vscode/src/configuration/ConnectionManager.ts
@@ -1,4 +1,9 @@
-import {Headers, WorkspaceClient, ApiClient, logging} from "@databricks/databricks-sdk";
+import {
+    Headers,
+    WorkspaceClient,
+    ApiClient,
+    logging,
+} from "@databricks/databricks-sdk";
 import {Cluster, WorkspaceFsEntity, WorkspaceFsUtils} from "../sdk-extensions";
 import {
     env,
