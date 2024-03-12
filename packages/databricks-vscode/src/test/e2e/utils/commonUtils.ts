e275cd9ab5f17fbb0404fa38b957ef023131ad0e
kartikgupta-db <kartik.gupta@databricks.com>
2024-03-12 20:09:03 +0100

    multiproc

--- packages/databricks-vscode/src/test/e2e/utils/commonUtils.ts
+++ packages/databricks-vscode/src/test/e2e/utils/commonUtils.ts
@@ -261,7 +261,7 @@ export async function waitForLogin(profileName: string) {
                 }
             }
         },
-        {timeout: 20_000, interval: 2_000, timeoutMsg: "Login didn't finish"}
+        {timeout: 60_000, interval: 2_000, timeoutMsg: "Login didn't finish"}
     );
 }
 
