41fea6d6b31923c1964314293eb553c29f13d171
kartikgupta-db <kartik.gupta@databricks.com>
2024-03-20 11:21:47 +0000

    parallel tests

--- packages/databricks-vscode/scripts/list_integration_tests.ts
+++ packages/databricks-vscode/scripts/list_integration_tests.ts
@@ -1,9 +1,14 @@
 import {glob} from "glob";
 import path from "path";
 
-const integrationTests = glob.sync(
-    path.join("src", "test", "e2e", "**", "*.e2e.ts")
-);
+const integrationTests = glob
+    .sync(path.join("src", "test", "e2e", "**", "*.e2e.ts"))
+    .map((testPath) => {
+        return {
+            path: testPath,
+            baseName: path.basename(testPath, ".e2e.ts"),
+        };
+    });
 
 // eslint-disable-next-line no-console
 console.log(JSON.stringify(integrationTests));
