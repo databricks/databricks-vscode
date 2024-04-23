e21845d3317c2781a59221ce3f317534bd61fdea
kartikgupta-db <kartik.gupta@databricks.com>
2024-04-23 16:29:11 +0200

    fix windows

--- packages/databricks-vscode/src/test/e2e/wdio.conf.ts
+++ packages/databricks-vscode/src/test/e2e/wdio.conf.ts
@@ -189,10 +189,15 @@ export const config: Options.Testrunner = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [["vscode", {cachePath: "/tmp/wdio-vscode-service"}]],
+    services: [
+        [
+            "vscode",
+            {cachePath: path.join(process.cwd(), "tmp", "wdio-vscode-service")},
+        ],
+    ],
 
     // cahePath above is for vscode binaries, this one is for the chromedriver
-    cacheDir: "/tmp/wdio-vscode-service",
+    cacheDir: path.join(process.cwd(), "tmp", "wdio-vscode-service"),
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
