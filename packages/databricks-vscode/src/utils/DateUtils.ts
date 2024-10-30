4479c80c12f6fb014b4488b9c293c30e7f30039b
Ilia Babanov <ilia.babanov@databricks.com>
2024-10-30 12:25:46 +0100

    Fix the start dates for job and pipeline runs (#1427)
    ## Changes
    Fix the start dates for job and pipeline runs
    
    ## Tests
    Unit

--- packages/databricks-vscode/src/utils/DateUtils.ts
+++ packages/databricks-vscode/src/utils/DateUtils.ts
@@ -3,10 +3,9 @@ export function toString(date: Date): string {
 }
 
 export function toDateString(date: Date): string {
-    const day = date.getDay();
+    const day = date.getDate();
     const month = date.toLocaleString("default", {month: "short"});
     const year = date.getFullYear();
-
     return `${day} ${month}, ${year}`;
 }
 
