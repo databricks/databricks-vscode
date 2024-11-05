96785b0e7f8ca23634c9201dc87c2fcaa7727f17
Ilia Babanov <ilia.babanov@databricks.com>
2024-11-05 11:34:42 +0100

    Attempt to fix the windows escaping

--- packages/databricks-vscode/src/test/e2e/wdio.conf.ts
+++ packages/databricks-vscode/src/test/e2e/wdio.conf.ts
@@ -35,12 +35,11 @@ const VSIX_PATH = path.resolve(
 );
 const VSCODE_STORAGE_DIR = path.resolve(tmpdir(), "user-data-dir");
 
-const metaCharsRegExp = /([()\][%!^"`<>&|;,*?])/g;
+const metaCharsRegExp = /([()\][%!^"`<>&|;, *?])/g;
 
 export function escapeCommand(arg: string): string {
     // Escape meta chars
-    arg = arg.replace(metaCharsRegExp, "^$1");
-
+    arg = `"${arg}"`.replace(metaCharsRegExp, "^$1");
     return arg;
 }
 
