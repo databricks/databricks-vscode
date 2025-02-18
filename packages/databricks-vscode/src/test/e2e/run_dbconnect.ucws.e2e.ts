2464d658b6520048201c75c6a13a8d9d87c3634b
Ilia Babanov <ilia.babanov@databricks.com>
2025-02-18 15:05:34 +0100

    Install setuptools instead of distutils

--- packages/databricks-vscode/src/test/e2e/run_dbconnect.ucws.e2e.ts
+++ packages/databricks-vscode/src/test/e2e/run_dbconnect.ucws.e2e.ts
@@ -26,7 +26,7 @@ describe("Run files on serverless compute", async function () {
 
         await fs.writeFile(
             path.join(projectDir, "requirements.txt"),
-            ["ipykernel", "distutils"].join("\n")
+            ["ipykernel", "setuptools"].join("\n")
         );
 
         await fs.writeFile(
