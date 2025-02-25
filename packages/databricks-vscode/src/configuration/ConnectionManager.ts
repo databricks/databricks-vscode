305c8334a7b5d74c33d3bbd9ac50877e1623f644
Ilia Babanov <ilia.babanov@databricks.com>
2025-02-25 13:23:14 +0100

    Always show serverless compute option in the cluster dropwodn
    Serverless detection for a workspace is not full proof.
    Instead of having false negatives, we show serverless option for everyone.
    
    Jobs and dbconnect runs already show clear errors if serverless is not supported.

--- packages/databricks-vscode/src/configuration/ConnectionManager.ts
+++ packages/databricks-vscode/src/configuration/ConnectionManager.ts
@@ -156,10 +156,7 @@ export class ConnectionManager implements Disposable {
         const serverless = await this.configModel.get("serverless");
         const computeId = this.apiClient?.config.serverlessComputeId;
         const autoEnable =
-            serverless === undefined &&
-            !this.cluster &&
-            computeId === "auto" &&
-            this.databricksWorkspace?.isServerlessEnabled;
+            serverless === undefined && !this.cluster && computeId === "auto";
         if (serverless || autoEnable) {
             await this.enableServerless();
         } else {
