60b030ece4dc6f91dd37cbf763ca24ee0077a4fe
Ilia Babanov <ilia.babanov@databricks.com>
2025-03-04 13:09:12 +0100

    Add OPEN_RESOURCE_EXTERNALLY telemetry event
    "openExternal" command is used for Bundle Resources and Docs panels.
    Resources already have "type" fields, I've added the same field for Docs panel items.
    
    New telemetry event has this type field, it should help us understand what resources people open externally,
    or what documentation guides people open more frequently than others.

--- packages/databricks-vscode/src/utils/UtilsCommands.ts
+++ packages/databricks-vscode/src/utils/UtilsCommands.ts
@@ -1,8 +1,11 @@
 import {Disposable, window, env} from "vscode";
 import {openExternal} from "./urlUtils";
+import {Events, Telemetry} from "../telemetry";
 
 export class UtilsCommands implements Disposable {
     private disposables: Disposable[] = [];
+
+    constructor(private telemetry: Telemetry) {}
 
     openExternalCommand() {
         return async (value: any | undefined) => {
@@ -19,6 +22,12 @@ export class UtilsCommands implements Disposable {
                     "Databricks: Can't open external link. No URL found."
                 );
                 return;
+            }
+            if (value.type !== undefined) {
+                this.telemetry.recordEvent(Events.OPEN_RESOURCE_EXTERNALLY, {
+                    type: value.type,
+                });
+                console.log("Tracked external resource", value.type);
             }
             await openExternal(url);
         };
