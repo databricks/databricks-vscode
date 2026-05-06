77ee483bdb1673d91feb84bb93dae20613e0f4dc
misha-db <misha.kezherashvili@databricks.com>
2026-05-06 13:45:37 +0400

    Split view optimizations

--- packages/databricks-vscode/resources/webview-ui/uc-detail.js
+++ packages/databricks-vscode/resources/webview-ui/uc-detail.js
@@ -487,7 +487,8 @@ function renderActions(data) {
         a.href = data.exploreUrl;
         a.innerHTML =
             `<svg viewBox="0 0 24 24"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/>` +
-            `<polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>Open in Databricks`;
+            `<polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>` +
+            `<span class="btn-label">Open in Databricks</span>`;
         linkContainer.appendChild(a);
     }
 }
